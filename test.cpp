bc6024517c43b6653f28926608942c35ff08c4f5
zhaoyue <tjandbjzy@foxmail.com>
2016-09-20 16:13:52 +0800

    .

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <map>
+#include <stack>
+#include <set>
+#include <ctime>
+
+typedef long long ll;
+
+int main(){
+
+    pintf("just test\n");
+    
+    return 0;
+}
